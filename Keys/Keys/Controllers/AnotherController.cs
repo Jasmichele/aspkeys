310759c984f5b86ea9f3684f6c04bd37e13df02b
Jasmichele <jasmichele@yahoo.com>
2016-10-27 09:08:56 -0400

    Lion

---
+++ Keys/Keys/Controllers/AnotherController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Keys.Controllers
+{
+    public class AnotherController : Controller
+    {
+        // GET: Another
+        public ActionResult Index(string name = "jas", int id = 3)
+        {
+            ViewBag.Name = name;
+            ViewBag.Id = id;
+            return View();
+        }
+
+        public ActionResult Yaaa()
+        {
+            ViewBag.Message = "Jasmine Rules The World";
+            return View();
+        }
+    }
+}
---
+++ Keys/Keys/Controllers/NextController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Keys.Controllers
+{
+    public class NextController : Controller
+    {
+        // GET: Next
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Loyalty Is The Most Important Key";
+            return View();
+        }
+    }
+}
